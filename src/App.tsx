756c6047f746fe62f7dd123781726530a0253845
Larissateruya28 <larissa.teruya@aluno.lsb.com.br>
2025-09-16 21:05:18 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      planos para sexta feira - 19 de setembro
+
+      <p>- bar de oniguiri </p>
+      <p>- bar de cookies </p>
+      <p>- filmes</p>
+      <p>- ebaaa</p>
+      <p>- descobrir o que o dico vai estar fazendo 👀</p>
+
+    </>
+  )
+}
+
+export default App
